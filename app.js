c724f93da5abe4896e4dfae1c44c0cda94088f73
Yash Vardhan <yashvardhan513@gmail.com>
2021-05-10 19:24:46 +0530

    code structure cleaning

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+
 
 const homeStartingContent = "This is a collabarative site for developers and coders who often and obviously get stuck at a point during their development journey.This platform can help those who are seeking answers and those who are able to give correct answers."
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -19,7 +18,7 @@ app.use(express.static("public"));
 
 
 
-mongoose.connect('mongodb+srv://admin-yash:Yash123@cluster0-1lje1.mongodb.net/ReEngineDB', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://admin-yash:Yash123@cluster0-1lje1.mongodb.net/ReEngineDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 
 const sellSchema = {
